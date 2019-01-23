2a77f1c1e3a059d013c7380493ab84c6c3c6d2d7
rparkerd33d <rparkerd33@gmail.com>
2019-01-23 09:15:23 -0600

    Updates

---
+++ precache-manifest.9885ef156bfeace7c0a426f1654995af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "5398730e9fccc83b9060",
+    "url": "/contactmanager/static/js/main.5398730e.chunk.js"
+  },
+  {
+    "revision": "5dc9fa4a246a172ab2a3",
+    "url": "/contactmanager/static/js/1.5dc9fa4a.chunk.js"
+  },
+  {
+    "revision": "5398730e9fccc83b9060",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "5dc9fa4a246a172ab2a3",
+    "url": "/contactmanager/static/css/1.0aef8ec9.chunk.css"
+  },
+  {
+    "revision": "87decb0cc7072947fbbe6210c29d7271",
+    "url": "/contactmanager/index.html"
+  }
+];
